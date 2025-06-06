0ced9cb71dede31d93f4b78ab1336d90f03b74c8
Holt570 <30250524@blackpool.ac.uk>
2025-06-06 04:12:50 +0100

    Final build
    - Improved commenting of preprocessing function
    - requires further development - datatype error in training function

--- predicted_grades_model.py
+++ predicted_grades_model.py
@@ -5,14 +5,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 model = RandomForestRegressor()
 df = pd.DataFrame()
 encoder = LabelEncoder()
+scaler = MinMaxScaler()
 
 # Preprocesses data ready for training and predictions
 def preprocess_data(df):
 
+    # calculate means to fill missing values
     social_media_mean = df["social_media_hours"].mean()
     netflix_mean = df["netflix_hours"].mean()
     attendance_mean = df["attendance_percentage"].mean()
@@ -20,6 +23,7 @@ def preprocess_data(df):
     exercise_mean = df["exercise_frequency"].mean()
     mental_health_mean = df["mental_health_rating"].mean()
 
+    #fill empty cells with avaraged or default values
     df.fillna({"age": 20}, inplace=True)
     df.fillna({"gender": "Other"}, inplace=True)
     df.fillna({"social_media_hours": social_media_mean}, inplace=True)
@@ -34,8 +38,10 @@ def preprocess_data(df):
     df.fillna({"mental_health_rating": mental_health_mean}, inplace=True)
     df.fillna({"extracurricular_participation": "No"}, inplace=True)
 
+    # remove rows without a student ID number
     df.dropna(subset = ["student_id"], inplace = True)
 
+    # correct invalid values
     for x in df.index:
         if df.loc[x, "study_hours_per_day"] == "varies":
             df.loc[x, "study_hours_per_day"] = 3
@@ -48,12 +54,17 @@ def preprocess_data(df):
 
     df.fillna({"study_hours_per_day": 3}, inplace=True)
 
+    # apply label encoding to non numerical values
     df['gender']= encoder.fit_transform(df['gender'])
     df['part_time_job']= encoder.fit_transform(df['part_time_job'])
     df['diet_quality']= encoder.fit_transform(df['diet_quality'])
     df['parental_education_level']= encoder.fit_transform(df['parental_education_level'])
     df['internet_quality']= encoder.fit_transform(df['internet_quality'])
     df['extracurricular_participation']= encoder.fit_transform(df['extracurricular_participation'])
+
+    #scaler.fit(df[["age","gender","study_hours_per_day","social_media_hours","netflix_hours","part_time_job","attendance_percentage","sleep_hours","diet_quality","exercise_frequency","parental_education_level","internet_quality","mental_health_rating","extracurricular_participation"]])
+    #scaled = scaler.fit_transform(df[["age","gender","study_hours_per_day","social_media_hours","netflix_hours","part_time_job","attendance_percentage","sleep_hours","diet_quality","exercise_frequency","parental_education_level","internet_quality","mental_health_rating","extracurricular_participation"]])
+    #normal_df = pd.DataFrame(scaled, columns=["age","gender","study_hours_per_day","social_media_hours","netflix_hours","part_time_job","attendance_percentage","sleep_hours","diet_quality","exercise_frequency","parental_education_level","internet_quality","mental_health_rating","extracurricular_participation"])
 
     result_text.insert(tk.END, f"Cleaning complete.")
 
@@ -80,12 +91,11 @@ def load_dataset(df):
     return None
 
 # Trains the Random Forest Regressor Model using the user input dataset, features and target
-def train_model(df, features, target):
+def train_model(model, df, features, target):
     try:
-        X = df[features]
+        x = df[features]
         y = df[target]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        model = RandomForestRegressor()
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
@@ -124,7 +134,7 @@ target_entry = tk.Entry(root)
 target_entry.pack(pady=5)
 
 # Creates button in GUI that calls train_model function
-train_button = tk.Button(root, text="Train Model", command=lambda: train_model(df, features_entry.get().split(','), target_entry.get()))
+train_button = tk.Button(root, text="Train Model", command=lambda: train_model(model, df, features_entry.get().split(','), target_entry.get()))
 train_button.pack(pady=10)
 
 # Creates button in GUI that calls make_predictions function
